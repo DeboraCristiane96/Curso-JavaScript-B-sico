e9741fdf044b210245b073c298eae7c006812eb6
DeboraCristiane96 <deboracristiane376@gmail.com>
2022-03-14 00:26:35 -0300

    Primeiros_Commits

---
+++ Aula006/script.js
@@ -0,0 +1,9 @@
+/*function funcao(){
+    console.log('funções e suas particularidades');
+}
+funcao();
+*/
+function mensagem(primeiro,segundo){
+    console.log(primeiro, segundo);
+}
+mensagem('Estudando','Javascript');
---
+++ Aula007/script.js
@@ -0,0 +1,18 @@
+/*function funcao(){
+    console.log('Cursando Javascript sintaxe básica');
+}
+funcao();
+
+var funcao = function mensagem(){
+    console.log('Aprofundando em funções');
+}
+funcao();
+var funcao = function(){
+    console.log('Aprofundando em funções');
+}
+funcao();
+*/
+var funcao = ()=> {
+    console.log('Arrow function')
+}
+funcao();
