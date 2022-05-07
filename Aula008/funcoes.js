c8aa7470265f5b4fcffbd1e6818cd3129238cc95
Débora Cristiane <101474273+DeboraCristiane96@users.noreply.github.com>
2022-05-07 15:14:02 -0300

    Update funcoes.js

--- Aula008/funcoes.js
+++ Aula008/funcoes.js
@@ -2,10 +2,10 @@ function returnEvenValues(array){
     let evensNum=[];
     for(let i = 0 ; i < array.length; i++){
         if(array[ i ] % 2 === 0){
-            evensNum.push(array[1]);
+            evensNum.push(array[i]);
         }
     }
-    console.log(evensNum);
+    console.log("Os números pares são:",evensNum);
 }
-let array = [1,3,6,8];
+let array = [1,3,6,8,10];
 returnEvenValues(array);
